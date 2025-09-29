7ced7cd548dcfb892ed16d13e37da9a6c70420a2
MatFragg <u202318323@upc.edu.pe>
2025-09-29 17:17:12 -0500

    refactor(iam:infrastructure):resolve the implementation of the infrastructure layer for the IAM bounded context

--- src/main/java/com/hampcoders/glottia/platform/api/iam/infrastructure/persistence/jpa/repositories/UserRepository.java
+++ src/main/java/com/hampcoders/glottia/platform/api/iam/infrastructure/persistence/jpa/repositories/UserRepository.java
@@ -1,4 +1,29 @@
 package com.hampcoders.glottia.platform.api.iam.infrastructure.persistence.jpa.repositories;
 
-public interface UserRepository {
+import com.hampcoders.glottia.platform.api.iam.domain.model.aggregates.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+/**
+ * This interface is responsible for providing the User entity related operations.
+ * It extends the JpaRepository interface.
+ */
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>
+{
+    /**
+     * This method is responsible for finding the user by username.
+     * @param username The username.
+     * @return The user object.
+     */
+    Optional<User> findByUsername(String username);
+
+    /**
+     * This method is responsible for checking if the user exists by username.
+     * @param username The username.
+     * @return True if the user exists, false otherwise.
+     */
+    boolean existsByUsername(String username);
 }
