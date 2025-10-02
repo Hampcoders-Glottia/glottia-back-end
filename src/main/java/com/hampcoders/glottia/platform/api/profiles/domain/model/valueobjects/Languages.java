ee0f6a4e4dd924e087c1e49f0fab1acec09c7800
Leandro Saul Contreras Lopez <leandro25.cl@gmail.com>
2025-10-02 14:36:56 -0500

    feat(profiles): add value objects for languages, CEFR levels, and street address

---
+++ src/main/java/com/hampcoders/glottia/platform/api/profiles/domain/model/valueobjects/Languages.java
@@ -0,0 +1,10 @@
+package com.hampcoders.glottia.platform.api.profiles.domain.model.valueobjects;
+
+public enum Languages {
+    ENGLISH,
+    SPANISH,
+    FRENCH,
+    GERMAN,
+    ITALIAN,
+    PORTUGUESE
+}
---
+++ src/main/java/com/hampcoders/glottia/platform/api/profiles/domain/model/valueobjects/NivelCefr.java
@@ -0,0 +1,12 @@
+package com.hampcoders.glottia.platform.api.profiles.domain.model.valueobjects;
+
+public enum NivelCefr {
+    A1,
+    A2,
+    B1,
+    B2,
+    C1,
+    C2,
+    NATIVE,
+    NONE
+}
