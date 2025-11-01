c2a17501cdfd7da2f78a33c2291044de67f1c75c
MatFragg <u202318323@upc.edu.pe>
2025-10-31 19:38:45 -0500

    refactor(glottia:profiles): update jpa repository methods
    Context: Command and Query services implementation could not match any of the declared methods on the JPA repositories.
    Decision: Renamed the repository methods to conform to the established JPA naming conventions.
    Impact: Ensures the Command and Query service implementations successfully resolve and execute the corresponding JPA repository methods.

--- src/main/java/com/hampcoders/glottia/platform/api/profiles/infrastructure/persistence/jpa/repository/LearnerRepository.java
+++ src/main/java/com/hampcoders/glottia/platform/api/profiles/infrastructure/persistence/jpa/repository/LearnerRepository.java
@@ -8,13 +8,12 @@ import org.springframework.stereotype.Repository;
 import com.hampcoders.glottia.platform.api.profiles.domain.model.entities.Learner;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface LearnerRepository extends JpaRepository<Learner, Long> {
-    List<Learner> findByCityIgnoreCase(String city);
-    List<Learner> findByCountryIgnoreCase(String country);
+    List<Learner> findByAddressCityIgnoreCase(String city);
+    List<Learner> findByAddressCountryIgnoreCase(String country);
     
-    @Query("SELECT DISTINCT l FROM Learner l JOIN l.learnerLanguages.learnerLanguageItems lli WHERE lli.language.id = :languageId")
+    @Query("SELECT DISTINCT l FROM Learner l JOIN l.learnerLanguage.learnerLanguageItems lli WHERE lli.language.id = :languageId")
     List<Learner> findByLearnerLanguageItemsLanguageId(@Param("languageId") Long languageId);
 }
